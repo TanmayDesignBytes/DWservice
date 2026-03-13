b44f0ff9956ffd70dcce2d123b96ce9711e57472
Tanmaypasalkar11 <pasalkar8899@gmail.com>
2026-03-13 14:51:02 +0530

    feat: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { fileURLToPath } from 'url'
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'src'),
+    },
+  },
+})
